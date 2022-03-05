469d2a1c9ac41a2589a11a07b1f80a53e13ce076
Emre Kardaşlar <emre.kardaslar@gmail.com>
2022-03-05 12:26:39 +0300

    add product-category class

--- src/main/java/com/emre/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/emre/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,29 @@ package com.emre.ecommerce.config;
 
 import com.emre.ecommerce.entity.Product;
 import com.emre.ecommerce.entity.ProductCategory;
+import javax.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -24,5 +39,27 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity ids
+        //
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- ui-ecommerce/src/app/services/product.service.ts
+++ ui-ecommerce/src/app/services/product.service.ts
@@ -2,21 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
   private baseUrl = 'http://localhost:8080/api/products';
-
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+  
   constructor(private http: HttpClient) { }
 
   getProductList(categoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
-    return this.http.get<GetResponse>(searchUrl).pipe(
+    return this.http.get<GetResponseProduct>(searchUrl).pipe(
       map(res => res._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.http.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(res => res._embedded.productCategory)
     );
   }
 
@@ -26,8 +33,14 @@ export class ProductService {
 }
 
 
-interface GetResponse {
+interface GetResponseProduct {
   _embedded: {
     products: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
